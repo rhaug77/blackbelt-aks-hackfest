d16ce734b29de6380f9e5d4ef8705d80616f5a0a
rhaug77 <rhaug77@gmx.net>
2018-07-12 19:15:08 -0700

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team
+        Romans Azure Global Blackbelt Team Change - Go Hawks
       </div>
       <div class="col-lg-6">
       </div>
